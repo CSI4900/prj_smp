831f456c65411b219c4bd497e982e15db6db1f05
Zechen <zhou2067522448@gmail.com>
2025-03-24 14:31:04 -0700

    Add valid_job.sh

---
+++ valid_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH --time=0-00:25:00      # Set a time limit
+#SBATCH --account=def-jyzhao   # Specify the account under which this job runs
+#SBATCH --mem=16000M           # Request 16GB of RAM
+#SBATCH --gpus-per-node=1      # Request 1 GPU per node
+#SBATCH --cpus-per-task=10     # Request 10 CPU cores per task
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK     # Set the number of OpenMP threads to match the allocated CPUs.
+
+
+# =====================================================
+# Batch Script for Compute Canada
+# Author:       Your Name
+# Date:         2025-02-06
+# Description:  This script runs a Python program using
+#               a virtual environment and submits a 
+#               SLURM job (run train.py).
+#
+# Usage:        $ sbatch train_job.sh
+#
+# Contributors: 
+# - Zechen Zhou     zzhou186@uottawa.ca
+# - Shun Hei Yiu    syiu017@uottawa.ca
+# =====================================================
+
+echo "Hello World"
+
+# Provide information and management capabilities for NVIDIA GPUs
+nvidia-smi
+
+# Load needed python and cuda modules
+module load python/3.12.4 cuda cudnn
+
+module load opencv/4.10.0
+
+# Activate your environment
+source ~/prj_smp_workspace/env/bin/activate
+
+# Upgrade pip
+# pip install --no-index -upgrade pip
+
+# Install packages
+# pip install --no-index -r requirement.txt
+
+# Variables for readability
+# logdir=/home/zzhou186/scratch/saved
+# datadir=/home/zzhou186/scratch/data
+# datadir=/prj_smp_workspace/training_data
+
+tensorboard --logdir=${logdir}/lightning_logs --host 0.0.0.0 --load_fast false & \
+    python ~/prj_smp_workspace/valid.py
+    # --model Conv \
+    # --batch_size 32 \
